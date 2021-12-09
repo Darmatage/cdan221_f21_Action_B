4f721169aca46563a8df2e95f8714136ad6f09d5
MichaelDentremont <91702010+MichaelDentremont@users.noreply.github.com>
2021-12-08 20:30:45 -0500

    Pause Menu and Music Manager wip

--- FA21ACTION_B/Assets/Scripts/GameHandler.cs
+++ FA21ACTION_B/Assets/Scripts/GameHandler.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour {
+
 
       private GameObject player;
       public static int playerHealth;
@@ -19,11 +21,59 @@ public class GameHandler : MonoBehaviour {
       //public static bool stairCaseUnlocked = false;
       //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
 
+        public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
+
+        void Awake (){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+
       void Start(){
+		   pauseMenuUI.SetActive(false);
             player = GameObject.FindWithTag("Player");
             playerHealth = StartPlayerHealth;
             updateStatsDisplay();       
       }
+
+        void Update (){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){
+                                Resume();
+                        }
+                        else{
+                                Pause();
+                        }
+                }
+        }
+
+        void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
+
+
+
+
 
       //public void playerGetTokens(int newTokens){
       //      gotTokens += newTokens;
@@ -73,6 +123,7 @@ public class GameHandler : MonoBehaviour {
       }
 
       public void RestartGame() {
+		  Time.timeScale = 1f;
             SceneManager.LoadScene("MainMenu");
             playerHealth = StartPlayerHealth;
       }
