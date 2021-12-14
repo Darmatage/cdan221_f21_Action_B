58b664d588ff86481e89c745dd3273bf92b2d4f3
JasonWiser <JasonWiserArt@gmail.com>
2021-12-13 19:45:29 -0500

    enemy work

--- FA21ACTION_B/Assets/Scripts/PickUp_Inventory.cs
+++ FA21ACTION_B/Assets/Scripts/PickUp_Inventory.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class PickUp_Inventory: MonoBehaviour {
 
-      public GameInventory CookBook;
-      public string ItemName = "item1";
+	public GameInventory CookBook;
+	public string ItemName = "item1";
 
-      void Awake(){
-            if (GameObject.FindWithTag("GameHandler") != null) {
-                  CookBook = GameObject.FindWithTag("GameHandler").GetComponent<GameInventory>();
-            }
-      }
+	public bool isJalepenos = false;
+	public int dmgJalepenos = 50;
 
-      void OnTriggerEnter2D(Collider2D other){
-            if (other.gameObject.tag == "Player"){
-                  //Debug.Log("You found an" + ItemName);
-				  GetComponent<Collider2D>().enabled = false;
-                  CookBook.InventoryAdd(ItemName);
-                  //CookBook.removeObjectFromLevel(ItemName);
-                  //Destroy(gameObject);
-            }
-      }
+	void Awake(){
+		if (GameObject.FindWithTag("GameHandler") != null) {
+			CookBook = GameObject.FindWithTag("GameHandler").GetComponent<GameInventory>();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player"){
+			//Debug.Log("You found an" + ItemName);
+			GetComponent<Collider2D>().enabled = false;
+			CookBook.InventoryAdd(ItemName);
+			//CookBook.removeObjectFromLevel(ItemName);
+			//Destroy(gameObject);
+		}
+			
+			//check for player
+		if (other.gameObject.tag == "Player"){
+			//Debug.Log("You found an" + ItemName);
+			GetComponent<Collider2D>().enabled = false;
+			CookBook.InventoryAdd(ItemName);
+			//CookBook.removeObjectFromLevel(ItemName);
+			//Destroy(gameObject);
+		}	
+		
+			//check for enemies
+		if (other.gameObject.tag == "enemyShooter"){
+			if (isJalepenos==true){
+				other.gameObject.GetComponent<EnemyMeleeDamage>().TakeDamage(dmgJalepenos); 
+			}
+			
+		}
+	}
+
 }
